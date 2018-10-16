0da768df4fb57684efaf1ff23f53c4efb174b104
anjua <anjua.github@gmail.com>
2018-10-16 07:52:27 +0700

    Testing

---
+++ views/layouts/login-layout.php
@@ -0,0 +1,42 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use backend\assets\LoginAsset;
+use yii\helpers\Html;
+
+LoginAsset::register($this);
+$asset = backend\assets\LoginAsset::register($this);
+$baseUrl = $asset->baseUrl;
+
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body class="login-page">
+<?php $this->beginBody() ?>
+<div class="wrapper">
+    <div class="container">
+        <?= $content ?>
+    </div>
+</div>
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">&copy; Anjua Partama Rusvandia <?= date('Y')?></p>
+        <p class="pull-right"><?= Yii::powered() ?></p>
+    </div>
+</footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
